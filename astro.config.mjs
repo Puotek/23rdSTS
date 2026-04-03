a769be00cc6506f3c59143a2c339ba98519b76f0
Puotek <hubertosz.p@gmail.com>
2026-04-03 04:41:04 +0200

    added cloudflare tunnel whitelist for dev testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,10 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
     site: "https://puotek.github.io",
     base: "/23rdSTS",
+    vite: {
+        server: {
+            allowedHosts: [".trycloudflare.com"],
+        },
+    },
+//   cloudflared tunnel --url http://localhost:4321
 });
